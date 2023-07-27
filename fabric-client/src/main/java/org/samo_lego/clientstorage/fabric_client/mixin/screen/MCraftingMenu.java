509b5619817f906cd3915abac327bce5c5687070
Amberichu <4722249+davchoo@users.noreply.github.com>
2023-07-27 05:19:21 -0400

    Remove unnecessary AScreenHandler

--- fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/mixin/screen/MCraftingMenu.java
+++ fabric-client/src/main/java/org/samo_lego/clientstorage/fabric_client/mixin/screen/MCraftingMenu.java
@@ -1,12 +1,10 @@
 package org.samo_lego.clientstorage.fabric_client.mixin.screen;
 
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.ContainerLevelAccess;
-import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.inventory.*;
 import org.samo_lego.clientstorage.fabric_client.event.ContainerDiscovery;
 import org.samo_lego.clientstorage.fabric_client.inventory.RemoteInventory;
 import org.samo_lego.clientstorage.fabric_client.inventory.RemoteSlot;
-import org.samo_lego.clientstorage.fabric_client.mixin.accessor.AScreenHandler;
 import org.samo_lego.clientstorage.fabric_client.mixin.accessor.ASlot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,21 +14,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static org.samo_lego.clientstorage.fabric_client.ClientStorageFabric.config;
 
 @Mixin(CraftingMenu.class)
-public class MCraftingMenu {
+public abstract class MCraftingMenu extends RecipeBookMenu<CraftingContainer> {
 
-    private final CraftingMenu self = (CraftingMenu) (Object) this;
+    public MCraftingMenu(MenuType<?> menuType, int i) {
+        super(menuType, i);
+    }
 
     @Inject(method = "<init>(ILnet/minecraft/world/entity/player/Inventory;Lnet/minecraft/world/inventory/ContainerLevelAccess;)V", at = @At("RETURN"))
     private void constructor(int syncId, Inventory playerInventory, ContainerLevelAccess context, CallbackInfo ci) {
         if (!config.enabled) return;
 
         // Moving slots down
-        self.slots.forEach(slot -> ((ASlot) slot).setY(slot.y + 36));
+        this.slots.forEach(slot -> ((ASlot) slot).setY(slot.y + 36));
 
         // Adding "remote slots"
         for (int row = 0; row < 3; ++row) {
             for (int col = 0; col < 9; ++col) {
-                ((AScreenHandler) self).cs_addSlot(new RemoteSlot(RemoteInventory.getInstance(), col + row * 9, col * 18 + 9, row * 18 - 23));
+                this.addSlot(new RemoteSlot(RemoteInventory.getInstance(), col + row * 9, col * 18 + 9, row * 18 - 23));
             }
         }
 
